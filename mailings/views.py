54910d62744ab352e45c2e755b6e144d29320434
Sergei Finogenov <serfinogenov@yandex.ru>
2025-06-27 23:10:40 +0300

    updating the templates for clients and adding method 'get_context_data' to the 'MailingRecipientListView'

--- mailings/views.py
+++ mailings/views.py
@@ -24,6 +24,11 @@ class MailingRecipientDetailView(DetailView):
 class MailingRecipientListView(ListView):
     model = MailingRecipient
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['recipients'] = MailingRecipient.objects.all()
+        return context
+
 
 class MailingRecipientUpdateView(UpdateView):
     model = MailingRecipient
