70814ada88c3929a15e6131a6d74a1f8a7742061
Sergei Finogenov <serfinogenov@yandex.ru>
2025-07-09 03:23:55 +0300

    updating code

--- users/management/commands/create_manager_group.py
+++ users/management/commands/create_manager_group.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
@@ -6,8 +6,11 @@ class Command(BaseCommand):
     help = 'Создание группы Менеджеры'
 
     def handle(self, *args, **kwargs):
-        group, created = Group.objects.get_or_create(name='Менеджеры')
+        managers_group, created = Group.objects.get_or_create(name='Менеджеры')
         if created:
+            add_perm = Permission.objects.get(codename='add_mailing')  # замените modelname на имя вашей модели
+            change_perm = Permission.objects.get(codename='change_mailing')
+            managers_group.permissions.add(add_perm, change_perm)
             self.stdout.write(self.style.SUCCESS('Группа "Менеджеры" создана.'))
         else:
             self.stdout.write(self.style.WARNING('Группа "Менеджеры" уже существует.'))
